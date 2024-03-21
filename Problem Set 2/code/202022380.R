01f7da23fd7bab5f5211afb9e79c78838bda5560
juanfwalteros <juanfrawalteros@gmail.com>
2024-03-20 22:43:40 -0500

    Final

--- Problem Set 2/code/202022380.R
+++ Problem Set 2/code/202022380.R
@@ -10,6 +10,7 @@ p_load(tidyverse,rio,skimr,janitor,data.table)
 
 rm(list=ls())
 
+
 #1. Importar/exportar bases de datos
 
 setwd("C:/Users/juanf/OneDrive - Universidad de los Andes/Octavo/Taller R/Problem Set 2/Problem-Set-2/Problem Set 2")
@@ -48,6 +49,7 @@ summary(location$p3053)
 location <- mutate(location,
                    ambulante=case_when(p3053>=3&p3053<6~1))
 
+
 #3. Eliminar filas/columnas de un conjunto de datos
 
 identification_sub <- select(identification, directorio,secuencia_p,secuencia_encuesta,
@@ -58,13 +60,20 @@ identification_sub <- select(identification, directorio,secuencia_p,secuencia_en
 location_sub <- select(location, directorio,secuencia_p,secuencia_encuesta,
                        ambulante,p3054,p469,cod_depto,f_exp)
 
+
 #4. Combinar bases de datos
 
 base_sub <- left_join(x = identification_sub, y = location_sub, by = c("directorio",
-                                                                       "secuencia_p","secuencia_encuesta"))
+                                                                       "secuencia_p",
+                                                                       "secuencia_encuesta",
+                                                                       "cod_depto"))
+export(base_sub,"output/base_sub.rds")
 
 #Aqui uni las bases con left_join tomando como x la base de identification_sub
 #Use left_join porque identification_sub es la base que contiene los datos base de los encuestados
+#Tambien use la variable cod_depto para unir las bases, dado que es una variable que esta en ambas bases
+#Asi solo me queda una unica cod_depto para la base unificada y la exporto a la carpeta output.
+
 
 #5. Descriptivas
 
@@ -75,32 +84,16 @@ skim(base_sub)
 summary(base_sub)
 #Summary me muestra minimos, cuartiles, medias, medianas, maximos y missing values para cada variable
 
+base_sub |> group_by(grupo_etario) |> summarize(puestos=mean(p3054,na.rm=T))
+base_sub |> group_by(cod_depto) |> summarize(puestos=mean(p3054,na.rm=T))
 
-### **2.1 Generales**
+base_sub |> group_by(ambulante) |> summarize(visible=mean(p469,na.rm=T))
 
-# Utilice summary para una descripción general 
-summary(geih$p6040)
-
-# select + summarize_all 
-select (.data=geih,
-        p6020,p6040,p6500) |> summarize_all(mean)
-
-### **2.2 Agrupadas**
-
-# ingreso laboral promedio por sexo
-geih |> group_by(p6020) |> 
-  summarise(ing_mean=mean(p6500,na.rm=T)) #group_by es categoria por la que agrupa info
-
-geih <- mutate(geih,mujer=ifelse(p6020==2,1,0))
-
-geih |> group_by(mujer) |> summarize(ing_mean=mean(p6500,na.rm=T))
-
-# ingreso laboral promedio por sexo y tipo de 
-
-geih |> group_by(mujer,p6450) |> summarize(ing_mean=mean(p6500,na.rm=T))
-
-# ingreso laboral promedio/mediano y años promedio en fondo de pension por sexo
-
-geih |> group_by(mujer,p6450) |> summarize(ing_mean=mean(p6500,na.rm=T),
-                                           med_edad=median(p6040,na.rm=T))
-
+#Con base en las estadisticas descriptivas halladas previamente, se encuentran resultados interesantes.
+#En primer lugar, se observa que los departamentos de Bolivar (13) y Antioquia (05) poseen, en promedio, 
+#la mayor cantidad de puestos u otras posesiones por micronegocio, con 1.09 y 1.07 respectivamente.
+#Por otro lado, agrupando por grupos etarios, se encuentra que los microempresarios de edades entre 45 y 56 años
+#son los que poseen menos puestos u otras posesiones importantes. Los que mas puestos poseen son los de edades entre
+#34 y 45 años. Por ultimo, hablando de los micronegocios ambulantes, se observa que estos son, en promedio, mas visibles
+#que los que no son ambulantes, donde los ambulantes tienen un valor de 1.15 y los no ambulantes de 1.38, donde si se es 
+#visible al publico se toma el valor de 1 y 2 de lo contrario.
